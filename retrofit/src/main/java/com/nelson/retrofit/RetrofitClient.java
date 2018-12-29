550b729ed0ee252673cf8278f921cb4b82adbdb4
Nelson <565265845@qq.com>
2018-12-29 14:15:10 +0800

    first commit

---
+++ retrofit/src/main/java/com/nelson/retrofit/RetrofitClient.java
@@ -0,0 +1,14 @@
+package com.nelson.retrofit;
+
+/**
+ * <pre>
+ *     author : Nelson
+ *     GitHub : https://github.com/Nelson-KK
+ *     time   : 2018/12/29
+ *     desc   :
+ * </pre>
+ */
+
+public class RetrofitClient {
+
+}
